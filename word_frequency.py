1d86d44a9a91631f5366b41cb3c9439dc7e52032
Ryan <rydowd@gmail.com>
2019-10-07 14:29:17 -0400

    ordered the printed list

--- word_frequency.py
+++ word_frequency.py
@@ -15,7 +15,7 @@ def remove_punctuation(string):
 
 def filter_stop_words(string, stop_words):
     """ Takes a depunctuated, lowercase string as the argument and splits it at spaces, then filters out any words that appear in the stop_words list"""
-    string_as_list = string.split(' ')
+    string_as_list = string.split()
     filtered_list = [word for word in string_as_list if word not in stop_words]
     return ' '.join(filtered_list)
       
@@ -23,7 +23,7 @@ def filter_stop_words(string, stop_words):
 def create_frequency_dictionary(string):
     """Takes a string and returns a dictionary where the keys are the words in that string and their values are the number of times that word occurs"""
     frequency_dictionary = {}
-    string_as_list = string.split(' ')
+    string_as_list = string.split()
     for word in string_as_list:
         frequency_dictionary[word] = frequency_dictionary.get(word, 0) + 1
     return frequency_dictionary
@@ -32,10 +32,19 @@ def create_frequency_dictionary(string):
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
+    def get_frequency(item):
+        return item[1]
+        
     read_file = open(file, 'r')
-    frequency_dictionary = (create_frequency_dictionary(filter_stop_words(remove_punctuation(read_file.read()), STOP_WORDS)))
+    punctuation_removed = remove_punctuation(read_file.read())
+    filtered = filter_stop_words(punctuation_removed, STOP_WORDS)
+    frequency_dictionary = create_frequency_dictionary(filtered)
+    dictionary_items = (frequency_dictionary.items())
+    sorted_frequency = sorted(dictionary_items, key=get_frequency, reverse=True)
+    for item in sorted_frequency:
+        print(item[0] + ' | ' + str(item[1]))
 
-print(print_word_freq('emancipation_proclamation.txt'))
+print_word_freq('seneca_falls.txt')
 
 
 # if __name__ == "__main__":
